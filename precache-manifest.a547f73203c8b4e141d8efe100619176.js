aa8c6a39fd9e18c1a1e6b8658627e491bcc952ed
ricurdofh <ricardo.servitechs@gmail.com>
2020-03-12 12:17:33 -0400

    Updates

---
+++ precache-manifest.a547f73203c8b4e141d8efe100619176.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ec984a2b985971722c8a78cc986a11",
+    "url": "/prueba-novateva/index.html"
+  },
+  {
+    "revision": "b5f162caf0838c3d294c",
+    "url": "/prueba-novateva/static/css/2.8c5e7601.chunk.css"
+  },
+  {
+    "revision": "d3c29ff97077e0edef05",
+    "url": "/prueba-novateva/static/css/main.b09562d2.chunk.css"
+  },
+  {
+    "revision": "b5f162caf0838c3d294c",
+    "url": "/prueba-novateva/static/js/2.d3b42410.chunk.js"
+  },
+  {
+    "revision": "55cbd54242e5f05163b329a995cd8626",
+    "url": "/prueba-novateva/static/js/2.d3b42410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c29ff97077e0edef05",
+    "url": "/prueba-novateva/static/js/main.35287037.chunk.js"
+  },
+  {
+    "revision": "eeeef08ab3f73a2cba54",
+    "url": "/prueba-novateva/static/js/runtime-main.8969a22f.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/prueba-novateva/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/prueba-novateva/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/prueba-novateva/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/prueba-novateva/static/media/notification.c0d3c94c.eot"
+  }
+]);
